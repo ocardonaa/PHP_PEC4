a4bb95054a652e9db0d9ea81309ff2c565c46c48
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-06-03 16:36:37 +0200

    first push

---
+++ PEC4/app/Models/Genre.php
@@ -0,0 +1,15 @@
+<?php
+/*
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class Genre extends Model
+{
+    protected $table = 'genres';
+    protected $primaryKey = 'genres';
+    public $timestamps = false;
+    public function videogames()
+    {
+        return $this->belongsToMany(Videogame::class, 'videogame_genres', 'genre', 'videogame_id');
+    }
+}
+    */
